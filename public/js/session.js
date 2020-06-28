1956fada6116e2dfa6324760b811e6f9fe8cd17f
vivek <vivek.appu619@gmail.com>
2020-06-28 09:01:53 +0530

    add book & add author appears only when logged in

--- public/js/session.js
+++ public/js/session.js
@@ -6,3 +6,19 @@ document.getElementById("logout").addEventListener("click",()=>{
     window.sessionStorage.setItem("token", null);
     location.reload();
 })
+let navitems=document.querySelectorAll(".nav-item");
+navitems=[...navitems]
+navitems.forEach(
+    (item)=>{
+       if(decoded){
+           item.classList.remove("hide");
+       }
+      
+    }
+)
+let edit=document.getElementById("edit");
+if(edit){
+    if(decoded){
+        edit.classList.remove("hide");
+    }
+}
