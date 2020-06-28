c0f66a4c72428f90b67b9b2d46ceeab6930f1caf
vivek <vivek.appu619@gmail.com>
2020-06-28 11:28:50 +0530

    added signin signup disappears when user is logged

--- public/js/session.js
+++ public/js/session.js
@@ -1,11 +1,5 @@
 var decoded = jwt_decode(window.sessionStorage.getItem('token'));
-if(decoded){
-    document.getElementById("logout").classList.remove("hide");
-}
-document.getElementById("logout").addEventListener("click",()=>{
-    window.sessionStorage.setItem("token", null);
-    location.reload();
-})
+
 let navitems=document.querySelectorAll(".nav-item");
 navitems=[...navitems]
 navitems.forEach(
@@ -16,6 +10,28 @@ navitems.forEach(
       
     }
 )
+
+if(decoded){
+    document.getElementById("logout").classList.remove("hide");
+    //don't show signin signup when logged
+    console.log("here")
+    document.querySelector(".navbar-nav .nav-item[href='/signin']").classList.add("hide");
+    document.querySelector(".navbar-nav .nav-item[href='/signup']").classList.add("hide");
+
+}
+
+document.getElementById("logout").addEventListener("click",()=>{
+    window.sessionStorage.setItem("token",null);
+    //throw out to index when logged out from editbook,editauthor,addbook,addauthor
+    if(window.location.pathname.split('/')[1]=="editbook"||window.location.pathname.split('/')[1]=="addbook"||window.location.pathname.split('/')[1]=="editauthor"||window.location.pathname.split('/')[1]=="addauthor"){
+       
+        window.location.href="/";
+    }
+    else{
+    location.reload();
+    }
+})
+
 let edit=document.getElementById("edit");
 if(edit){
     if(decoded){
